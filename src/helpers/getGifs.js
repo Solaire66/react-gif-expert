fdae429e5c0c9712ea16383493cce3e5ee8e5222
Ricardo <rcavazosg@gmail.com>
2023-05-14 12:10:31 -0600

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=gpuASAJpCSuYUIG4JDJN1cZwexZTcOlS&q=${ category }&limit=5`
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+
+
+    return gifs;
+}
+//MVe4pktd9xIRBBxR48IBEnLOaZ4fwrcK - https://api.giphy.com/v1/gifs/search?api_key=gpuASAJpCSuYUIG4JDJN1cZwexZTcOlS&q=${ category }&limit=10
